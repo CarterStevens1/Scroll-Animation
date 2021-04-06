74caeaba4d231ae5397094f84605f9e3f8bb04a7
CarterStevens1 <CarterStevens1@users.noreply.github.com>
2021-04-06 11:47:00 +0100

    comments

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const boxes = document.querySelectorAll(".box");
-
+//scroll event listener
 window.addEventListener("scroll", checkBoxes);
 
 checkBoxes();
-
+//checks the height of the page
 function checkBoxes() {
   const triggerBottom = (window.innerHeight / 5) * 4;
-
+  //checks the box top of the element
   boxes.forEach((box) => {
     const boxTop = box.getBoundingClientRect().top;
-
+    //if loop to add or remove show class
     if (boxTop < triggerBottom) {
       box.classList.add("show");
     } else {
